3f2f2ad4028a6eaae8ebde85b6d3cf967ae6f9cf
RobertJing <jack.jing7@gmail.com>
2016-08-18 17:12:21 -0400

    level one code test

--- angular2-quickstart/app/main.ts
+++ angular2-quickstart/app/main.ts
@@ -1,3 +1,22 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>{{title}}</h1>
+    <h2>{{carPart.name}}</h2>
+    <p>{{carPart.description}}</p>
+    <p>{{carPart.inStock}} in Stock</p>`
+})
+
+class AppComponent {
+    title = 'Ultra Racing';
+    carPart = {
+        "id": 1,
+        "name": "Super Tires",
+        "description": "These tires are the very best",
+        "inStock": 5
+    }
+}
+
+bootstrap(AppComponent);
