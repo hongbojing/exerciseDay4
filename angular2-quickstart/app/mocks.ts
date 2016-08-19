72eacd56186baa5f86d520821248045bda282311
RobertJing <jack.jing7@gmail.com>
2016-08-19 16:09:16 -0400

    end of level 3, split over, could move on

---
+++ angular2-quickstart/app/mocks.ts
@@ -0,0 +1,25 @@
+import { CarPart } from './car-part';
+
+export const CARPARTS: CarPart[] = [
+    {
+        "id": 1,
+        "name": "Super Tires",
+        "description": "These tires are the very best",
+        "inStock": 0,
+        "price": 4.99
+    },
+    {
+        "id": 2,
+        "name": "Super * 2 Tires",
+        "description": "22 is double the amount of 11",
+        "inStock": 0,
+        "price": 12.99
+    },
+    {
+        "id": 3,
+        "name": "3 * Super Tires",
+        "description": "Three people walking around has my teacher",
+        "inStock": 0,
+        "price": 2.99
+    }
+];
