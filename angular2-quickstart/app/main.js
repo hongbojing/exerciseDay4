7ca2753fc252e47b6b754ab0a84cdc142006022b
RobertJing <jack.jing7@gmail.com>
2016-08-19 13:35:49 -0400

    code school level 2 and add one more ng if at the top

--- angular2-quickstart/app/main.js
+++ angular2-quickstart/app/main.js
@@ -13,17 +13,42 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Ultra Racing';
-        this.carPart = {
-            "id": 1,
-            "name": "Super Tires",
-            "description": "These tires are the very best",
-            "inStock": 5
-        };
+        this.carParts = [
+            {
+                "id": 1,
+                "name": "Super Tires",
+                "description": "These tires are the very best",
+                "inStock": 0,
+                "price": 4.99
+            },
+            {
+                "id": 2,
+                "name": "Super * 2 Tires",
+                "description": "22 is double the amount of 11",
+                "inStock": 0,
+                "price": 12.99
+            },
+            {
+                "id": 3,
+                "name": "3 * Super Tires",
+                "description": "Three people walking around has my teacher",
+                "inStock": 0,
+                "price": 2.99
+            }
+        ];
     }
+    AppComponent.prototype.totalCarParts = function () {
+        var sum = 0;
+        for (var _i = 0, _a = this.carParts; _i < _a.length; _i++) {
+            var carPart = _a[_i];
+            sum += carPart.inStock;
+        }
+        return sum;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n    <h2>{{carPart.name}}</h2>\n    <p>{{carPart.description}}</p>\n    <p>{{carPart.inStock}} in Stock</p>"
+            template: "<h1>{{title}}</h1>\n    <p *ngIf=\"totalCarParts() > 0\">There are {{totalCarParts()}} total parts in stock.</p>\n    <p *ngIf=\"totalCarParts() === 0\">It is out of stock in stock.</p>\n\n    <ul *ngFor=\"let carPart of carParts\">\n        <li>\n            <h2>{{carPart.name | uppercase}}</h2>\n            <p>{{carPart.description}}</p>\n            <p>{{carPart.price | currency:'CAD':true}}\n            <p *ngIf=\"carPart.inStock > 0\">{{carPart.inStock}} in Stock</p>\n            <p *ngIf=\"carPart.inStock === 0\">Out of stock</p>\n        </li>\n    </ul>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- angular2-quickstart/app/main.ts
+++ angular2-quickstart/app/main.ts
@@ -4,19 +4,65 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-    <h2>{{carPart.name}}</h2>
-    <p>{{carPart.description}}</p>
-    <p>{{carPart.inStock}} in Stock</p>`
+    <p *ngIf="totalCarParts() > 0">There are {{totalCarParts()}} total parts in stock.</p>
+    <p *ngIf="totalCarParts() === 0">It is out of stock in stock.</p>
+
+    <ul *ngFor="let carPart of carParts">
+        <li>
+            <h2>{{carPart.name | uppercase}}</h2>
+            <p>{{carPart.description}}</p>
+            <p>{{carPart.price | currency:'CAD':true}}
+            <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
+            <p *ngIf="carPart.inStock === 0">Out of stock</p>
+        </li>
+    </ul>`
 })
 
 class AppComponent {
     title = 'Ultra Racing';
-    carPart = {
+    carParts = [
+    {
         "id": 1,
         "name": "Super Tires",
         "description": "These tires are the very best",
-        "inStock": 5
+        "inStock": 0,
+        "price": 4.99
+    },
+    {
+        "id": 2,
+        "name": "Super * 2 Tires",
+        "description": "22 is double the amount of 11",
+        "inStock": 0,
+        "price": 12.99
+    },
+    {
+        "id": 3,
+        "name": "3 * Super Tires",
+        "description": "Three people walking around has my teacher",
+        "inStock": 0,
+        "price": 2.99
     }
+    ];
+    totalCarParts() {
+        let sum = 0;
+        for (let carPart of this.carParts) {
+            sum += carPart.inStock;
+        }
+
+        return sum;
+    }
+    /*
+    totalCarParts() {
+        return this.carParts.reduce(function(prev, current){
+            return prev + current.inStock;
+        }, 0)
+    }
+
+    totalCarParts() {
+        return this.carParts.reduce((prev, current) => prev + current.inStock, 0);
+    }
+    */
+
 }
 
 bootstrap(AppComponent);
